2e06f2af24d1ad2f6f081cdccb13a85dce725163
tttenko <tttenko37@gmail.com>
2025-02-02 15:14:37 +0300

    Create connection to DB

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,21 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/schema_name";
+    private static final String ROOT = "root";
+    private static final String PASSWORD = "Qqwweerrttyy2003";
+
+    private static void getConnection() {
+        try (Connection connection = DriverManager.getConnection(URL, ROOT, PASSWORD)) {
+            if (!connection.isClosed()) {
+                System.out.println("Connection success!");
+            }
+        } catch (SQLException e) {
+            System.out.println("Not connection! " + e.getMessage());
+        }
+    }
 }
