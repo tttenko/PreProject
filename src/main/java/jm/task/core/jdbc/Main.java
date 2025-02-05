1fc1acf24c4ccacc262a64da5574dbff27fa50e5
tttenko <tttenko37@gmail.com>
2025-02-05 13:12:24 +0300

    Оптимизация импортов

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,37 +1,19 @@
 package jm.task.core.jdbc;
-
-import jm.task.core.jdbc.dao.UserDao;
-import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
-import org.hibernate.Session;
+
 
 public class Main {
     public static void main(String[] args) {
-//        Util.connectionJDBC();
-//        UserDao userDao = new UserDaoJDBCImpl();
-//
-//        userDao.createUsersTable();
-//
-//        userDao.saveUser("Name1", "LastName1", (byte) 20);
-//        userDao.saveUser("Name2", "LastName2", (byte) 25);
-//        userDao.saveUser("Name3", "LastName3", (byte) 31);
-//        userDao.saveUser("Name4", "LastName4", (byte) 38);
-//
-//        userDao.removeUserById(1);
-//
-//        userDao.dropUsersTable();
-//        System.out.println(userDao.getAllUsers());
-//        userDao.cleanUsersTable();
 
         Util.connectionHibernate();
         UserServiceImpl userService = new UserServiceImpl();
         userService.dropUsersTable();
         userService.createUsersTable();
-        userService.saveUser("Max", "Kop", 32);
-//        User user = userService.getAllUsers().get(0);
-//        System.out.println(user.toString());
+        userService.saveUser("Max", "Kop", 21);
+        User user = userService.getAllUsers().get(0);
+        System.out.println(user.toString());
+
     }
 }
